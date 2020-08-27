0eee6da934822c64982042968e8c8a90dafad3d6
saikiran rudra <saikiranrudra2@gmail.com>
2020-08-27 11:17:14 +0530

    small layout twicks

--- src/pages/admin/Staff.js
+++ src/pages/admin/Staff.js
@@ -17,77 +17,81 @@ import { ReactComponent as Logout } from "./../../assets/dashboardAssets/logout.
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  container: {
-    display: "grid",
-    gridTemplateColumns: "90px 1fr 1fr 1fr 1fr",
-    gridTemplateRows: "58px 1fr",
-    gridGap: ".3rem",
-    height: "100vh",
-    backgroundColor: "#fff",
-  },
-  menuContainer: {
-    display: "flex",
-    flexDirection: "row",
-    gridRow: "span 2",
-    justifyContent: "center",
-  },
-  logo: {
-    width: "4rem",
-    marginTop: "1rem",
-  },
+    container: {
+        display: "grid",
+        gridTemplateColumns: "90px 350px 350px 350px 350px",
+        gridTemplateRows: "58px 1fr",
+        gridGap: ".5rem",
+        height: "100vh",
+        backgroundColor: "#fff",
+    },
+    menuContainer: {
+        display: "flex",
+        flexDirection: "row",
+        gridRow: "span 2",
+        justifyContent: "center",
+    },
+    logo: {
+        width: "4rem",
+        marginTop: "1rem",
+    },
 });
 
 const Staff = () => {
-  const classes = useStyles();
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [takeAttendence, setTakeAttendence] = useState(false);
-  return (
-    <div className={classes.container}>
-      <Paper className={classes.menuContainer}>
-        <div style={{ width: "100%", textAlign: "center" }}>
-          <img src={logo} alt="logo" className={classes.logo} />
-          <Nav />
-        </div>
-      </Paper>
+    const classes = useStyles();
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [takeAttendence, setTakeAttendence] = useState(false);
+    return (
+        <div className={classes.container}>
+            <Paper className={classes.menuContainer}>
+                <div style={{ width: "100%", textAlign: "center" }}>
+                    <img src={logo} alt="logo" className={classes.logo} />
+                    <Nav />
+                </div>
+            </Paper>
 
-      <div
-        style={{ gridColumn: "2 / -1", display: "flex", alignItems: "center" }}
-      >
-        <span style={{ width: "8rem", margin: "0 auto" }}></span>
-        {/* <img
+            <div
+                style={{
+                    gridColumn: "2 / -1",
+                    display: "flex",
+                    alignItems: "center",
+                }}
+            >
+                <span style={{ width: "8rem", margin: "0 auto" }}></span>
+                {/* <img
           src={logo}
           alt="logo"
           style={{ width: "8rem", margin: "0 auto" }}
         /> */}
-        <Button
-          variant="contained"
-          color="primary"
-          endIcon={<Logout style={{ width: "1rem", fill: "#fff" }} />}
-          style={{ margin: "1rem" }}
-        >
-          Logout
-        </Button>
-      </div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    endIcon={<Logout style={{ width: "1rem", fill: "#fff" }} />}
+                    style={{ margin: "1rem" }}
+                >
+                    Logout
+                </Button>
+            </div>
 
-      <StaffCalender
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-      />
-      {takeAttendence ? (
-        <TakeStaffAttendence
-          selectedDate={selectedDate}
-          setTakeAttendence={setTakeAttendence}
-        />
-      ) : (
-        <StaffDisplay
-          setTakeAttendence={setTakeAttendence}
-          selectedDate={selectedDate}
-        />
-      )}
-      <StaffSpecificAttendence />
-      <EditStaff />
-    </div>
-  );
+            <StaffCalender
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+            />
+            {takeAttendence ? (
+                <TakeStaffAttendence
+                    selectedDate={selectedDate}
+                    setTakeAttendence={setTakeAttendence}
+                />
+            ) : (
+                <StaffDisplay
+                    setTakeAttendence={setTakeAttendence}
+                    selectedDate={selectedDate}
+                />
+            )}
+            <StaffSpecificAttendence />
+            <EditStaff />
+        </div>
+    );
 };
 
 export default Staff;
